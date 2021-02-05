d1f10e1314026534fa97e24678aef48837006ee6
jethoo <jeewanbhusal@gmail.com>
2021-02-04 23:17:19 -0500

    LoginForm added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,42 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 
 const App = () => {
+  //states
+  const [ username, setUsername ] = useState('')
+  const [ password, setPassword ] = useState('')
+  const [ user, setUser ] = useState(null)
+  
+  //loginForm
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>      
+  )
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    console.log('username is ', username, 'password is', password)
+  }
+
   const [blogs, setBlogs] = useState([])
 
   useEffect(() => {
@@ -13,6 +49,7 @@ const App = () => {
 
   return (
     <div>
+      { loginForm() }
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
