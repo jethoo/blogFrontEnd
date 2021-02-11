1590ddaa56ac72969a1dea8aaba69aade7f79d4c
jethoo <jeewanbhusal@gmail.com>
2021-02-10 22:52:57 -0500

    Form change event handler testing added

--- src/components/BlogForm.test.js
+++ src/components/BlogForm.test.js
@@ -10,12 +10,14 @@ test('<BlogForm /> form calls event handler with right details', () => {
   const component = render(
     <BlogForm blogObject={blogObject} />
   )
-  component.debug()
-  const title = component.container.querySelector('input')
-  const author = component.container.querySelector('input:nth-child(1)')
-  const url = component.container.querySelector('input:nth-child(2)')
+
+  //referencing three input fields of blogform
+  const title = component.container.querySelector('input:nth-child(1)')
+  const author = component.container.querySelector('input:nth-child(2)')
+  const url = component.container.querySelector('input:nth-child(3)')
   const form = component.container.querySelector('form')
 
+  //with the change event , supplying values to each input fields
   fireEvent.change(title, {
     target: { value: 'Testing Form fullstackopen' }
   })
@@ -25,10 +27,11 @@ test('<BlogForm /> form calls event handler with right details', () => {
   fireEvent.change(url, {
     target: { value: 'www.test.com' }
   })
-
+  //submitting the form
   fireEvent.submit(form)
-
-  //expect(createBlog.mock.calls).toHaveLength(1)
-  console.log('blogObject', blogObject.mock)
-  //expect(blogObject.mock.calls[0][0].title).toBe('Testing Form fullstackopen')
+  //expecting above supplied values to be rightly placed in the blog created
+  expect(blogObject.mock.calls).toHaveLength(1)
+  expect(blogObject.mock.calls[0][0].title).toBe('Testing Form fullstackopen')
+  expect(blogObject.mock.calls[0][0].author).toBe('JB')
+  expect(blogObject.mock.calls[0][0].url).toBe('www.test.com')
 })
